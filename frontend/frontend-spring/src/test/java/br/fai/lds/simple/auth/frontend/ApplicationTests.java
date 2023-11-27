01dc6c3fd953b39c5433ff6a1154624c194cb6c8
Gabriel Porto <7gabrielporto@gmail.com>
2023-11-26 21:31:05 -0300

    Initial commit

---
+++ frontend/frontend-spring/src/test/java/br/fai/lds/simple/auth/frontend/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.fai.lds.simple.auth.frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
