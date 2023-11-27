d5e5b4f894bfc63684407050c4084de409019337
Gabriel Porto <7gabrielporto@gmail.com>
2023-11-26 23:31:52 -0300

    refactor: refactor LoginUseCase

--- backend/backend-usecases/src/main/java/br/fai/lds/backend/usecases/authentication/LoginUseCase.java
+++ backend/backend-usecases/src/main/java/br/fai/lds/backend/usecases/authentication/LoginUseCase.java
@@ -1,8 +1,18 @@
 package br.fai.lds.backend.usecases.authentication;
 
+import br.fai.lds.backend.usecases.port.LoginRepository;
 import br.fai.lds.domain.UserModel;
+import br.fai.lds.domain.port.JwtService;
 
 public class LoginUseCase {
+
+    private final LoginRepository loginRepository;
+    private final JwtService jwtService;
+
+    public LoginUseCase(LoginRepository loginRepository, JwtService jwtService) {
+        this.loginRepository = loginRepository;
+        this.jwtService = jwtService;
+    }
 
     public UserModel login(final String email, final String password) {
         if(email.isEmpty() || password.isEmpty()) {
