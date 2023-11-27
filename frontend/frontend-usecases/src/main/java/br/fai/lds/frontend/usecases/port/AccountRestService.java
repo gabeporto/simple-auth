6a55110352ebbad1274167597b4a44993cf5144a
Gabriel Porto <7gabrielporto@gmail.com>
2023-11-27 00:34:05 -0300

    refactor: refactor layout

--- frontend/frontend-usecases/src/main/java/br/fai/lds/frontend/usecases/port/AccountRestService.java
+++ frontend/frontend-usecases/src/main/java/br/fai/lds/frontend/usecases/port/AccountRestService.java
@@ -3,6 +3,5 @@ package br.fai.lds.frontend.usecases.port;
 import br.fai.lds.domain.UserModel;
 
 public interface AccountRestService {
-
     UserModel validateCredentials(final String email, final String password);
 }
