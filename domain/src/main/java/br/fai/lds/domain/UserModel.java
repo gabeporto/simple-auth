c339757a2c2f977f93ce7cd369b72598a57bfa24
Gabriel Porto <7gabrielporto@gmail.com>
2023-11-26 21:36:26 -0300

    feat: add UserModel on domain

---
+++ domain/src/main/java/br/fai/lds/domain/UserModel.java
@@ -0,0 +1,14 @@
+package br.fai.lds.domain;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor
+public class UserModel {
+
+    private int id;
+    private String email;
+    private String password;
+}
