24eed26c3904a600a3135b014201bd6e6f7648d7
Gabriel Porto <7gabrielporto@gmail.com>
2023-11-27 10:35:41 -0300

    feat: add advice api message with duck api

--- backend/backend-usecases/src/main/java/br/fai/lds/backend/usecases/duck/RandomDuckUseCase.java
+++ backend/backend-usecases/src/main/java/br/fai/lds/backend/usecases/duck/RandomDuckUseCase.java
@@ -1,17 +1,49 @@
 package br.fai.lds.backend.usecases.duck;
 
 import br.fai.lds.domain.dto.DuckDto;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 public class RandomDuckUseCase {
-    private String externalApiUrl = "https://random-d.uk/api/v2";
+    private String duckApiUrl = "https://random-d.uk/api/v2/random";
+    private String adviceApiUrl = "https://api.adviceslip.com/advice";
+
     public DuckDto getRandomDuck() {
-        String apiUrl = externalApiUrl + "/random";
+        try {
+            DuckDto duckDto = new DuckDto();
 
-        RestTemplate restTemplate = new RestTemplate();
-        DuckDto randomDuck = restTemplate.getForObject(apiUrl, DuckDto.class);
+            RestTemplate restTemplate = new RestTemplate();
+            ResponseEntity<DuckDto> duckResponseEntity = restTemplate.getForEntity(duckApiUrl, DuckDto.class);
 
-        return randomDuck;
+            if (duckResponseEntity.getStatusCode().is2xxSuccessful()) {
+                DuckDto duckApiDto = duckResponseEntity.getBody();
+                duckDto.setUrl(duckApiDto.getUrl());
+            } else {
+                System.err.println("Erro na resposta da API de Duck: " + duckResponseEntity.getStatusCode());
+                return null;
+            }
+
+            ResponseEntity<String> adviceResponseEntity = restTemplate.getForEntity(adviceApiUrl, String.class);
+
+            if (adviceResponseEntity.getStatusCode().is2xxSuccessful()) {
+                String adviceResponseBody = adviceResponseEntity.getBody();
+
+                ObjectMapper objectMapper = new ObjectMapper();
+                JsonNode adviceJsonNode = objectMapper.readTree(adviceResponseBody);
+
+                String advice = adviceJsonNode.path("slip").path("advice").asText();
+                duckDto.setMessage(advice);
+            } else {
+                System.err.println("Erro na resposta da API de Conselhos: " + adviceResponseEntity.getStatusCode());
+                return null;
+            }
+
+            return duckDto;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
-
